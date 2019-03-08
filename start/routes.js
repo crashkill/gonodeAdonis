88f843f20d111b46df7a5c6e8509dabddf8aef3b
crashkill <crash.kill@gmail.com>
2019-03-08 01:13:16 -0300

    Crud de projetos e Tarefas

--- start/routes.js
+++ start/routes.js
@@ -6,5 +6,11 @@ Route.post('users', 'UserController.store')
 Route.post('sessions', 'SessionController.store')
 Route.post('password', 'ForgotPasswordController.store')
 Route.put('password', 'ForgotPasswordController.update')
-Route.post('files', 'FileController.store')
+
 Route.get('/files/:id', 'FileController.show')
+
+Route.group(() => {
+  Route.post('files', 'FileController.store')
+  Route.resource('projects', 'ProjectController').apiOnly()
+  Route.resource('projects.tasks', 'TaskController').apiOnly()
+}).middleware(['auth'])
